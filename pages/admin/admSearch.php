178223c53c30e535ca4ba8fae36c058a0b85bb83
ib2394 <ibtisamhfz@gmail.com>
2024-06-17 02:33:33 +0800

    betulkan bahagian admin
    tambah gambar kat signupAdm,
    just betul betulkan sikit bahagian admin yg lain

--- pages/admin/admSearch.php
+++ pages/admin/admSearch.php
@@ -27,7 +27,8 @@ include ('../../config/config.php');
                 border-radius: 20px;
                 border: transparent;
                 align-items: center;
-                box-shadow: 0 10px 25px rgba(0, 0, 0, 0.3)
+                box-shadow: 0 10px 25px rgba(0, 0, 0, 0.3);
+                position: center;
             }
             .container box{
                 display: flex;
@@ -35,55 +36,59 @@ include ('../../config/config.php');
                 align-items: center;
                 padding: 1rem 2rem;
             }
+
         </style>
     </head>
     <body>
         <div class="container my-5">
             <form method="POST">
                 <input class="box" type="test" placeholder="Search Here..." name="search">
-                <button class="btn btn-dark btn-sm" name="submit">Search</button>
             </form>
-
-            <div class="container my-5">
-                <table class="table">
-                    <?php
-                        if(isset($_POST['submit'])){
-                            $search=$_POST['search'];
-                            $sql="SELECT * FROM parcel WHERE courname LIKE '%$search%' OR size LIKE '%$search%' OR status LIKE '%$search%' OR payid LIKE '%$search%' ";
-                            $result=mysqli_query($con, $sql);
-                            if($result){
-                                if(mysqli_num_rows($result)>0){
-                                    echo '<thead>
-                                    <tr>
-                                        <th>Parcel ID</th>
-                                        <th>Courier</th>
-                                        <th>Size</th>
-                                        <th>Status</th>
-                                        <th>Student ID</th>
-                                        <th>Pay ID</th>
-                                    </tr>
-                                    </thead>';
-
-                                    while($row=mysqli_fetch_assoc($result)){;
-                                        echo '<tbody>
+            <div class="row print-container">
+                <div class="container my-5">
+                    <table class="table">
+                        <?php
+                            if(isset($_POST['search'])){
+                                $search=$_POST['search'];
+                                $sql="SELECT * FROM parcel WHERE courname LIKE '%$search%' OR size LIKE '%$search%' OR status LIKE '%$search%' OR payid LIKE '%$search%' ";
+                                $result=mysqli_query($con, $sql);
+                                if($result){
+                                    if(mysqli_num_rows($result)>0){
+                                        echo '<thead>
                                         <tr>
-                                            <td>'.$row['parcelid'].'</td>
-                                            <td>'.$row['courname'].'</td>
-                                            <td>'.$row['size'].'</td>
-                                            <td>'.$row['status'].'</td>
-                                            <td>'.$row['studid'].'</td>
-                                            <td>'.$row['payid'].'</td>
+                                            <th>Parcel ID</th>
+                                            <th>Courier</th>
+                                            <th>Size</th>
+                                            <th>Status</th>
+                                            <th>Student ID</th>
+                                            <th>Pay ID</th>
                                         </tr>
-                                        </tbody>';
+                                        </thead>';
+
+                                        while($row=mysqli_fetch_assoc($result)){;
+                                            echo '<tbody>
+                                            <tr>
+                                                <td>'.$row['parcelid'].'</td>
+                                                <td>'.$row['courname'].'</td>
+                                                <td>'.$row['size'].'</td>
+                                                <td>'.$row['status'].'</td>
+                                                <td>'.$row['studid'].'</td>
+                                                <td>'.$row['payid'].'</td>
+                                            </tr>
+                                            </tbody>';
+                                        }
+                                    }
+                                    else{
+                                        echo '<h2 class=:text-danger>Data not found</h2>';
                                     }
                                 }
-                                else{
-                                    echo '<h2 class=:text-danger>Data not found</h2>';
-                                }
                             }
-                        }
-                    ?>
-                </table>
+                        ?>
+                    </table>
+                    <div class="text-center">
+                            <button onclick="window.print()" class="btn btn-primary">Print</button>
+                    </div>
+                </div>
             </div>
 
         </div>
